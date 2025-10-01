1d1c971a560e8cca924fe1bc5d18cd5b1a32ffa0
Fabián Murgado <fabian@fabianmurgado.com>
2025-10-01 16:15:55 +0200

    feat: implement basic complex number class

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+use std::ops::{Add,AddAssign};
+
+#[derive(Debug, Clone, Copy)]
+struct Complex {
+    re: f64,
+    im: f64,
+}
+
+impl Complex {
+    fn magnitude(&self) -> f64 {
+        (self.re * self.re + self.im * self.im).sqrt()
+    }
+
+    fn magnitude_squared(&self) -> f64 {
+        self.re * self.re + self.im * self.im
+    }
+}
+
+impl Add for Complex {
+    type Output = Complex;
+
+    fn add(self, other: Complex) -> Complex {
+        Complex {
+            re: self.re + other.re,
+            im: self.im + other.im,
+        }
+    }
+}
+
+impl AddAssign for Complex {
+    fn add_assign(&mut self, other: Complex) {
+        self.re += other.re;
+        self.im += other.im;
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
